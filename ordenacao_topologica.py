31fb7275f1ff73df9d8e8b72bf532e28c90fa638
iconoclasst <davi09bezerra70@gmail.com>
2026-04-15 15:22:32 -0300

    add files

---
+++ ordenacao_topologica.py
@@ -0,0 +1,58 @@
+from collections import deque
+
+# branco = 0
+# cinza  = 1
+# preto  = 2
+
+class vertice:
+    def __init__(self, nome):
+        self.nome=nome
+        self.d = 0
+        self.f = 0
+        self.p = None
+        self.cor=0
+
+    def print_v(self):
+        print(self.nome)
+
+def ordenacao_topologica(grafo):
+    tempos = []
+    tempo=0
+    for u in grafo:
+        if u.cor == 0:
+            dfs_visit(grafo, u, tempo, tempos)
+    return tempos
+
+def dfs_visit(grafo, u, tempo, tempos):
+    tempo+=1
+    u.d=tempo
+    u.cor=1
+    for v in grafo[u]:
+        if v.cor==0:
+            dfs_visit(grafo, v, tempo, tempos)
+    u.cor=2
+    tempo+=1
+    u.f=tempo
+    tempos.append((u.nome, u.f))
+
+a = vertice('a')
+b = vertice('b')
+c = vertice('c')
+d = vertice('d')
+e = vertice('e')
+f = vertice('f')
+
+
+grafo = {
+    a:[d, f],
+    b:[e],
+    c:[e, f],
+    d:[a],
+    e:[b, c, f],
+    f:[c, e]
+}
+
+tempos = dfs(grafo)
+
+tempos = sorted(tempos, key=lambda x: x[1], reverse=True)
+print(tempos)
